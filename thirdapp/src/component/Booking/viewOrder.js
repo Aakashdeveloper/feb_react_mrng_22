fa29f07b8cb6c713d30bd3b94e4a0dac27cfc721
aakashdeveloper <ahanda205@gmail.com>
2022-03-10 07:28:40 +0530

    orders

--- thirdapp/src/component/Booking/viewOrder.js
+++ thirdapp/src/component/Booking/viewOrder.js
@@ -1,12 +1,30 @@
 import React, {Component} from 'react'
+import axios from 'axios';
+import DisplayOrder from './displayOrder';
+
+const url = "http://localhost:6700/orders"
 
 class ViewOrder extends Component {
+    constructor(props){
+        super(props)
+
+        this.state={
+            orders:''
+        }
+    }
 
     render(){
         return(
-            <h1>View OrderP</h1>
+           <>
+            <DisplayOrder orderData={this.state.orders}/>
+           </>
         )
     }
+
+    componentDidMount(){
+        axios.get(url).then((res)=>{this.setState({orders:res.data})})
+    }
+
 }
 
 export default ViewOrder
